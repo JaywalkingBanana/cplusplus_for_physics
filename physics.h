ebabda2070d4d718fc4cc1b80630bba6a083ed57
JaywalkingBanana <62420073+JaywalkingBanana@users.noreply.github.com>
2021-01-16 17:20:53 +0100

    Create physics.h

---
+++ physics.h
@@ -0,0 +1,10 @@
+#ifndef _physics_h
+#define _physics_h
+
+namespace Physics{
+  static const double h_bar = 6.64 * pow(10, -34);
+  static const double epsilon_0 = 8.8541878 * pow(10, -12);
+  static const double e = 1.6021766 * pow(10, -19);
+  static const double g = 6.67408 * pow(10, -11);
+};
+#endif
